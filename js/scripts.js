e79fa3f169b558cfc8539d72215455419aedf093
Anthony Bottemiller and Jackson Meyer <anthony@>
2016-08-18 10:49:44 -0700

    adds ability to parse entered numbers to exactly matching single roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
+var parseNumeral = function(inputNumber) {
+  var numeralArray=["1","5","10","50","100","500","1000"];
+  var romNumeralArray=["I","V","X","L","C","D","M"];
+  console.log(inputNumber)
+  for (i=0; i<numeralArray.length; i++){
+    if (inputNumber === numeralArray[i]) {
+      return romNumeralArray[i];
+    };
+  };
+  return "please enter a better number";
+
+};
+
+
+
+
+
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     console.log("Button Clicked");
     var inputNumber = $("#input").val();
-    console.log(inputNumber);
+    console.log(parseNumeral(inputNumber));
   });
 });
